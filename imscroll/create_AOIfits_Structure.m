3276501f174d34fb05720193e7e9db1f62760e8f
leejuyuu <leejuyuu@gmail.com>
2019-05-05 22:33:06 +0800

    move some code from FitAOIs_call back to create_AOIfits_Structure.m

---
+++ imscroll/create_AOIfits_Structure.m
@@ -0,0 +1,36 @@
+function aoifits = create_AOIfits_Structure(handles)
+% Define the output structure of the AOIfits data
+
+folder=handles.TiffFolder;
+% next four lines because 'folder' cannot be empty, or we get an error
+% message when referencing folder(1,:) in build_mapstruc.m
+[aa, bb]=size(folder);
+if (aa==0)&&(bb==0);
+    folder='folder not specified';
+end
+
+
+aoiWidth = str2double(get(handles.PixelNumber,'String')); % Fetch the pixel number (aoi width)
+
+frameAverage = round(str2double(get(handles.FrameAve,'String')));
+
+aoiinf = handles.FitData; 
+
+aoifits.dataDescription='[aoinumber framenumber amplitude xcenter ycenter sigma offset integrated_aoi (integrated pixnum) (original aoi#)]';
+aoifits.parameter=[frameAverage aoiWidth];
+aoifits.parameterDescription='[ave pixnum]';
+aoifits.tifFile=folder;
+aoifits.centers=aoiinf(:,3:4);
+aoifits.centersDescription='[aoi_xcenters aoi_ycenters]';
+aoifits.aoiinfo2Description='[(framenumber when marked) ave x y pixnum aoinumber]';
+aoifits.aoiinfo2=handles.FitData;
+aoifits.AllSpotsDescription='aoifits.AllSpots{m,1}=[x y] spots in frm m;  {m,2}=# of spots,frame m; {m,3}=frame #; {1,4}=[firstframe:lastframe]; {2,4}=NoiseDiameter  SpotDiameter  SpotBrightness]'  ;
+'{2,4}=NoiseDiameter  SpotDiameter  SpotBrightness]'  ;
+aoifits.AllSpots=FreeAllSpotsMemory(handles.AllSpots);
+aoifits.FarPixelDistance=handles.FarPixelDistance;      % See MapButton callback
+aoifits.NearPixelDistance=handles.NearPixelDistance;    % case 20 and case 21
+aoifits.Refaoiinfo2=handles.Refaoiinfo2;                % to see what these
+aoifits.RefAOINearLogik=handles.RefAOINearLogik;        % are for. (part of background subtraction method)
+aoifits.RefAOINearLogikDesc=' e.g. Bkgndaoifits.aoiinfo2(aoifits.RefAOINearLogik{12},:) for AOIs near to reference AOI #12 in aoifits.Refaoiinfo2';
+
+end
--- imscroll/imscroll.m
+++ imscroll/imscroll.m
@@ -324,6 +324,8 @@ elseif ischar(varargin{1}) % INVOKE NAMED SUBFUNCTION OR CALLBACK
 end
 
 
+
+
 %| ABOUT CALLBACKS:
 %| GUIDE automatically appends subfunction prototypes to this file, and 
 %| sets objects' callback properties to call them through the FEVAL 
@@ -1261,64 +1263,10 @@ function FitAOIs_Callback(hObject, eventdata, handles, varargin)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 %global argouts imageset folderpass %parenthandles
-
-
-
-
-%dum=varargin{1};
-%images=varargin{2};
-%folder=varargin{3};
-
-folder=handles.TiffFolder;
-% next four lines because 'folder' cannot be empty, or we get an error
-% message when referencing folder(1,:) in build_mapstruc.m
-[aa, bb]=size(folder);
-if (aa==0)&&(bb==0);
-    folder='folder not specified';
-end
-
-%folderpass=folder;
-pixnum=str2double(get(handles.PixelNumber,'String')); % Fetch the pixel number (aoi width)
-
-ave=round(str2double(get(handles.FrameAve,'String')));
-
-aoiinf=handles.FitData;                         % AOIs selected earlier (AOI button, tag=CollectAOI)
-%[framenumber ave x y pixnum aoinumber];
-
-% Now successively fit each AOI over the
-
-% specified frame range
-
-
-% Define most of the output structure
-aoifits.dataDescription='[aoinumber framenumber amplitude xcenter ycenter sigma offset integrated_aoi (integrated pixnum) (original aoi#)]';
-aoifits.parameter=[ave pixnum];
-aoifits.parameterDescription='[ave pixnum]';
-aoifits.tifFile=folder;
-aoifits.centers=aoiinf(:,3:4);
-aoifits.centersDescription='[aoi_xcenters aoi_ycenters]';
-aoifits.aoiinfo2Description='[(framenumber when marked) ave x y pixnum aoinumber]';
-aoifits.aoiinfo2=handles.FitData;
-aoifits.AllSpotsDescription='aoifits.AllSpots{m,1}=[x y] spots in frm m;  {m,2}=# of spots,frame m; {m,3}=frame #; {1,4}=[firstframe:lastframe]; {2,4}=NoiseDiameter  SpotDiameter  SpotBrightness]'  ;
-'{2,4}=NoiseDiameter  SpotDiameter  SpotBrightness]'  ;
-aoifits.AllSpots=FreeAllSpotsMemory(handles.AllSpots);
-aoifits.FarPixelDistance=handles.FarPixelDistance;      % See MapButton callback
-aoifits.NearPixelDistance=handles.NearPixelDistance;    % case 20 and case 21
-aoifits.Refaoiinfo2=handles.Refaoiinfo2;                % to see what these
-aoifits.RefAOINearLogik=handles.RefAOINearLogik;        % are for. (part of background subtraction method)
-aoifits.RefAOINearLogikDesc=' e.g. Bkgndaoifits.aoiinfo2(aoifits.RefAOINearLogik{12},:) for AOIs near to reference AOI #12 in aoifits.Refaoiinfo2';
-outputName=get(handles.OutputFilename,'String');
-
-
-
-% *** filler_for_imscroll_fit_aois_callback  placed here   ****
-
-
-
-mapstruc2d=build_2d_mapstruc_aois_frms(handles);        % Build a 2D mapstruc to direct data processing
-
-
-
+aoifits = create_AOIfits_Structure(handles);
+outputName = get(handles.OutputFilename,'String');
+ % Build a 2D mapstruc to direct data processing
+mapstruc2d = build_2d_mapstruc_aois_frms(handles);       
 DataOutput2d=gauss2d_mapstruc2d_v2(mapstruc2d,handles); % Process the data (integrate, fit etc)
 % V.2 is parallel processing
 
