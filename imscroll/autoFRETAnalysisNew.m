6bc7b385e302bcc00324ff2754b427d40a9ee54a
leejuyuu <leejuyuu@gmail.com>
2019-07-07 21:30:02 +0800

    update the fret analysis function so that it checks the aois are not out of bound

--- imscroll/autoFRETAnalysisNew.m
+++ imscroll/autoFRETAnalysisNew.m
@@ -8,7 +8,7 @@ for iFile = 2:nFile + 1
     input = struct;
     A = load([dataDir, parametersIn{iFile,1},'_driftfit.dat'],'-mat');
     ImagePath = A.aoifits.tifFile;
-    
+    A = load([dataDir, parametersIn{iFile,1},'_driftlist.dat'],'-mat');
     input.driftlist = A.driftlist;
     A = load([dataDir, parametersIn{iFile,1},'_aoi.dat'],'-mat');
     input.aois = A.aoiinfo2;
@@ -20,18 +20,21 @@ for iFile = 2:nFile + 1
     aoifits = create_AOIfits_Structure_woHandle(ImagePath,input.aois);
     imageFileProperty = getImageFileProperty(ImagePath);
     aoiinfo = input.aois;
-    shiftedXY = batchShitfAOI(aoiinfo,aoiProcessParameters.frameRange,input.driftlist);
-    [~,aoiinfo] = removeOutOfEdgeAOIs(shiftedXY,aoiinfo,imageFileProperty);
-    
-    aoiinfo(:,[3 4]) = mapXY(aoiinfo(:,[3 4]),mapMatrix);
-    shiftedXY = batchShitfAOI(aoiinfo,aoiProcessParameters.frameRange,input.driftlist);
-    [~,aoiinfo] = removeOutOfEdgeAOIs(shiftedXY,aoiinfo,imageFileProperty);
-    
     aoiProcessParameters = struct(...
         'frameRange',(parametersIn{iFile,2}:parametersIn{iFile,3}),...
         'frameAverage', parametersIn{iFile,4}...
         );
-        
+    shiftedXY = batchShitfAOI(aoiinfo(:,[3 4]),aoiinfo(1,1),...
+        aoiProcessParameters.frameRange,input.driftlist);
+    [~,aoiinfo] = removeOutOfEdgeAOIs(shiftedXY,aoiinfo,imageFileProperty);
+    
+    aoiinfo(:,[3 4]) = mapXY(aoiinfo(:,[3 4]),mapMatrix);
+    shiftedXY = batchShitfAOI(aoiinfo(:,[3 4]),aoiinfo(1,1),...
+        aoiProcessParameters.frameRange,input.driftlist);
+    [~,aoiinfo] = removeOutOfEdgeAOIs(shiftedXY,aoiinfo,imageFileProperty);
+    
+    
+    
     pc = getAoiIntensityLinearInterpWithBackground(imageFileProperty,aoiinfo,aoiProcessParameters,input.driftlist);
     aoifits.dataGreen = pc.ImageData;
     aoifits.backgroundTraceGreen = pc.BackgroundTrace;
@@ -52,7 +55,7 @@ for iFile = 2:nFile + 1
     traces = struct(...
         'donor',traceGreen,...
         'acceptor',traceRed,...
-        'total',traceToTal,...
+        'total',traceTotal,...
         'FRET',FRETtrace...
         );
     save([dataDir, parametersIn{iFile,1},'_FRETtraces.dat'],'traces');
