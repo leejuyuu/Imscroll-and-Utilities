94f47f8c0ae0c86ffbb7f7a180dc216ea3b3b7d6
leejuyuu <leejuyuu@gmail.com>
2019-07-22 09:50:54 +0800

    auto analysis for non cropped images

--- imscroll/autoAnalysisNew.m
+++ imscroll/autoAnalysisNew.m
@@ -13,7 +13,7 @@ for iFile = 2:nFile + 1
     driftfit = A.aoifits;
     
     %{
-    input.driftlist = A.driftlist;
+    driftlist = A.driftlist;
     %}
     A = load([dataDir, parametersIn{iFile,1},'_aoi.dat'],'-mat');
     input.aois = A.aoiinfo2;
@@ -37,14 +37,41 @@ save([dataDir, parametersIn{iFile,1},'_driftparam.dat'],'param');
         'frameRange',(parametersIn{iFile,2}:parametersIn{iFile,3}),...
         'frameAverage', parametersIn{iFile,4}...
         );
-    shiftedXY = batchShitfAOI(aoiinfo,aoiProcessParameters.frameRange,driftlist);
+    shiftedXY = batchShitfAOI(aoiinfo(:,[3 4]),aoiinfo(1,1),...
+        aoiProcessParameters.frameRange,driftlist);
     [~,aoiinfo] = removeOutOfEdgeAOIs(shiftedXY,aoiinfo,imageFileProperty);
-    pc = getAoiIntensityLinearInterpWithBackground(imageFileProperty,aoiinfo,aoiProcessParameters,driftlist);
-    aoifits.data = pc.ImageData;
-    aoifits.backgroundTrace = pc.BackgroundTrace;
-    aoifits.beforeBackground = pc.beforeBackground;
-    save([dataDir, parametersIn{iFile,1},'_intcorrected.dat'],'aoifits');
     
+    aoiinfo(:,[3 4]) = mapXY(aoiinfo(:,[3 4]),getInverseMap(mapMatrix));
+    shiftedXY = batchShitfAOI(aoiinfo(:,[3 4]),aoiinfo(1,1),...
+        aoiProcessParameters.frameRange,driftlist);
+    [~,aoiinfo] = removeOutOfEdgeAOIs(shiftedXY,aoiinfo,imageFileProperty);
+    
+    
+    
+    pc = getAoiIntensityLinearInterpWithBackground(imageFileProperty,aoiinfo,aoiProcessParameters,driftlist);
+    aoifits.dataRed = pc.ImageData;
+    aoifits.backgroundTraceRed = pc.BackgroundTrace;
+    aoifits.beforeBackgroundRed = pc.beforeBackground;
+    
+    
+    %%
+    aoiinfo(:,[3 4]) = mapXY(aoiinfo(:,[3 4]),mapMatrix);
+    pc = getAoiIntensityLinearInterpWithBackground(imageFileProperty,aoiinfo,aoiProcessParameters,driftlist);
+    aoifits.dataGreen = pc.ImageData;
+    aoifits.backgroundTraceGreen = pc.BackgroundTrace;
+    aoifits.beforeBackgroundGreen = pc.beforeBackground;
+    
+    save([dataDir, parametersIn{iFile,1},'_intcorrected.dat'],'aoifits');
+    nAOIs = length(aoiinfo(:,1));
+    traceGreen = reshape(aoifits.dataGreen(:,8),nAOIs,[]);
+    traceRed = reshape(aoifits.dataRed(:,8),nAOIs,[]);
+    
+    traces = struct(...
+        'green',traceGreen,...
+        'red',traceRed...
+        );
+    save([dataDir, parametersIn{iFile,1},'_traces.dat'],'traces');    
+    %%
     highParameters = struct(...
         'noiseDiameter',1,...
         'spotDiameter',5,...
@@ -55,14 +82,19 @@ save([dataDir, parametersIn{iFile,1},'_driftparam.dat'],'param');
         'spotDiameter',5,...
         'spotBightness',parametersIn{iFile,6}...
         );
-    region = {1 imageFileProperty.width 1 imageFileProperty.height};
+    region = {257 imageFileProperty.width 1 imageFileProperty.height};
     AllSpotsHigh = FindAllSpots(imageFileProperty,region,aoiProcessParameters,highParameters);
     AllSpotsLow = FindAllSpots(imageFileProperty,region,aoiProcessParameters,lowParameters);
     
     radius = 1.5;
     radius_hys = 1.5;
-    shiftedXY = batchShitfAOI(aoiinfo,aoiProcessParameters.frameRange,input.driftlist);
+    shiftedXY = batchShitfAOI(aoiinfo(:,[3 4]),aoiinfo(1,1),...
+        aoiProcessParameters.frameRange,driftlist);
     IntervalDataStructure = SpotIntervals(aoiProcessParameters.frameRange,...
         radius,radius_hys,aoiinfo,shiftedXY,AllSpotsHigh,AllSpotsLow,input.time');
     save([dataDir, parametersIn{iFile,1},'_interval.dat'],'IntervalDataStructure');
+    
+    
+end
+fprintf('done\n');
 end
