7db19b8e97a59a901b6e7205670f677638044e93
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-01 16:26:17 +0800

    Duplicate getAOIsubImageAndCenter.m
    Duplicate this function to be able to modify and use it without breaking
    the autoAnalysisGlimpseMultichannel function. This is going to cause
    some problems, but hopefully I will merge the two files back soon.

---
+++ imscroll/getAOIsubImageAndCenterDuplicate.m
@@ -0,0 +1,19 @@
+function [subimage, newcenter] = getAOIsubImageAndCenterDuplicate(image, center, radius)
+[ymax,xmax] = size(image);
+edge(1,:) = center - radius;
+edge(2,:) = center + radius;
+y = edge(3):edge(4);
+x = edge(1):edge(2);
+newcenter = [radius+1, radius+1];
+
+newcenter(2) = newcenter(2)-sum(y<1);
+y = y(y>=1);
+y = y(y < ymax);
+
+newcenter(1) = newcenter(1)-sum(x<1);
+x = x(x >= 1);
+x = x(x < xmax);
+
+subimage = image(y,x);
+
+end
