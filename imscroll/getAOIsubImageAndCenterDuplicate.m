0b4d1b887e7bb4594af4925474a6bf3ee21ff3e8
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-01 21:28:26 +0800

    Refactor: getAOIsubImageAndCenterDuplicate
    - Refine the variable naming
    - Clean up unused comments
    - Doc

--- imscroll/getAOIsubImageAndCenterDuplicate.m
+++ imscroll/getAOIsubImageAndCenterDuplicate.m
@@ -1,21 +1,22 @@
-function [subimage, neworigin] = getAOIsubImageAndCenterDuplicate(image, center, radius)
+function [subimage, neworigin] = getAOIsubImageAndCenterDuplicate(image, center, aoiHalfwidth)
 % Get the AOI sub image given the AOI center coordinate and AOI halfwidth.
 % 
+% Input args: 
+%     image: M x N array, the image
+%     center: 1 x 2 float, the center coordinate of the aoi
+%     aoiHalfwidth: fload, the half width of the aoi
+%
+% Output args:
+%     subimage: (2 * aoiHalfwidth) x (2 * aoiHalfwidth) image array
+%     neworigin: 1 x 2 int, the indices of the top-left pixel in the 
+%                subimage relative to the original image
+
 [ymax,xmax] = size(image);
-edge(1,:) = round(center - radius + 0.5);
-edge(2,:) = round(center + radius - 0.5);
-y = edge(3):edge(4);
-x = edge(1):edge(2);
+edge(1,:) = round(center - aoiHalfwidth + 0.5);
+edge(2,:) = round(center + aoiHalfwidth - 0.5);
+yRange = edge(3):edge(4);
+xRange = edge(1):edge(2);
 neworigin = edge(1, :);
-% 
-% newcenter(2) = newcenter(2)-sum(y<1);
-% y = y(y>=1);
-% y = y(y < ymax);
-% 
-% newcenter(1) = newcenter(1)-sum(x<1);
-% x = x(x >= 1);
-% x = x(x < xmax);
 
-subimage = image(y,x);
-
+subimage = image(yRange,xRange);
 end
