426bc883c22b657b225e65e50c48bd6e52799d05
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-02 19:56:55 +0800

    Add a test for MakeMappingFile function
    Test it against a previously produced result. This will allow me to
    change without messing the behavior.

---
+++ imscroll/test/testMapping.m
@@ -0,0 +1,14 @@
+function tests = testMapping
+tests = functiontests(localfunctions);
+end
+
+function testMakeMappingFile(testCase)
+loaded = load('imscroll/test/test_data/20200206_br_02_field1.dat', '-mat');
+field1 = loaded.aoiinfo2;
+loaded = load('imscroll/test/test_data/20200206_br_02_field2.dat', '-mat');
+field2 = loaded.aoiinfo2;
+loaded = load('imscroll/test/test_data/20200206_br_02.dat', '-mat');
+map = MakeMappingFile(field1, field2, 'fitparm_test.dat');
+verifyEqual(testCase, map.fitparmvector, loaded.fitparmvector);
+verifyEqual(testCase, map.mappingpoints, loaded.mappingpoints);
+end
