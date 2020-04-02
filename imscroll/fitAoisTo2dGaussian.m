ce2456e061837df1e936eccb907e3bdc5d68c19f
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-02 17:33:56 +0800

    Clean up fitAoisTo2dGaussian
    - Refine variable naming
    - Modify the progress printing statement
    - Comments and documentation

--- imscroll/fitAoisTo2dGaussian.m
+++ imscroll/fitAoisTo2dGaussian.m
@@ -1,57 +1,97 @@
-function pc = fitAoisTo2dGaussian(aoiinfo,...
+function data = fitAoisTo2dGaussian(aoiinfo,...
                                   aoiProcessParameters,...
                                   imageFileProperty,...
                                   isTrackAOI)
+% Fit the AOIs in the aoiinfo list to 2D gaussian function over a frame 
+% range and return the fitted coefficients. 
+% f(x, y) = amplitude * exp(-((x-x0).^2 + (y-y0).^2) / (2*sigma^2)) + offset
+% 
+% The major use case of this function is to get the x, y coordinates of 
+% AOIs. This is used when updating to more precise coordinates or to track
+% the AOIs.
+%
+% Input args:
+%     aoiinfo: N x 6 array, the information of the aoi got from imscroll
+%              handles.FitData. The columns correspond to 
+%              [(framenumber when marked), frameAverage, x, y, aoiWidth, aoinumber]
+%     aoiProcessParameters: struct with two fields
+%         {
+%         frameAverage: int, number of frames to average
+%                       when processing image
+%         frameRange: 1 x M int, the frame numbers the processing to
+%                     iterate over
+%         }
+%     imageFileProperty: struct with 3 fields {
+%         nFrame: the total number of frames in the image sequence
+%         width: the image width (pixel)
+%         height: the image height (pixel)
+%         fileType: str, specify the image type, possible 'tiff' or 
+%                   'glimpse'
+%         filePath: str, the path to the image (tiff) or image directory 
+%                   glimpse
+%         gheader: the vid structure if the fileType is glimpse
+%         }
+%     isTrackAOI: bool, determine whether the user want to update the
+%                 starting fitting parameter (x0, y0) after every frame 
+%                 to the previous frame (true) or just use the aoiinfo as 
+%                 the starting parameter (false)
+%
+% Output arg:
+%     data: N*M x 8 array, the info from the fitted result of each frame
+%           and each AOI. The AOI number changes faster then the frame
+%           number when you go down the rows. The columns correspond to 
+%           [aoi#, frame#, amplitude, x0, y0, sigma, offset, total intensity of AOI]
+%
 nAOI = length(aoiinfo(:, 1));
 nFrame = length(aoiProcessParameters.frameRange);
-
-% Pre-Allocate space
-ImageDataParallel = zeros(nAOI, 8, nFrame);
-
 frameAverage = aoiProcessParameters.frameAverage;
 
-%Now loop through the remaining frames
-for framemapindx=1:nFrame
-    if framemapindx/10==round(framemapindx/10)
-        framemapindx
+% Pre-Allocate space
+tempData = zeros(nAOI, 8, nFrame);
+
+for iFrame = 1:nFrame
+    if mod(iFrame,10) == 0
+        %indicate the current progress
+        fprintf('processing frame %d\n',iFrame)
     end
     % Get the averaged image of this frame to process
-    currentFrameNumber = aoiProcessParameters.frameRange(framemapindx);
-    
-    currentfrm = getAveragedImage(...
+    currentFrameNumber = aoiProcessParameters.frameRange(iFrame);
+    currentFrameImage = getAveragedImage(...
         imageFileProperty,...
         currentFrameNumber,...
         frameAverage);
-    for aoiindx2=1:nAOI   % Loop through all the aois for this frame
+    
+    for iAOI = 1:nAOI
         
-        if isTrackAOI && framemapindx ~= 1 && framemapindx ~= 2
-            coord = ImageDataParallel(aoiindx2,4:5,framemapindx - 1);
+        if isTrackAOI && iFrame ~= 1 && iFrame ~= 2
+            % Use the coordinate from the fitted result in the last frame
+            coord = tempData(iAOI,4:5,iFrame - 1);
         else
-            coord = aoiinfo(aoiindx2, 3:4);
+            % Use the coordinate pre-determined for all frame
+            coord = aoiinfo(iAOI, 3:4);
         end
         
-        pixnum = aoiinfo(aoiindx2, 5);
-        [currentaoi, aoi_origin] = getAOIsubImageAndCenterDuplicate(currentfrm, coord, pixnum/2);
+        aoiWidth = aoiinfo(iAOI, 5);
+        [currentAoiImage, aoi_origin] = getAOIsubImageAndCenterDuplicate(currentFrameImage, coord, aoiWidth/2);
         
-        inputarg0 = guessStartingParameters(double(currentaoi));
+        startingCoeff = guessStartingParameters(double(currentAoiImage));
         
-        % Now fit the current aoi
-        outarg=gauss2dfit(double(currentaoi),double(inputarg0));
+        % Fit the current aoi
+        fittedCoeff = gauss2dfit(double(currentAoiImage),double(startingCoeff));
         
-        % ImageDataParallel: [aoi#     frm#       amp    xo    yo    sigma  offset (int inten)]
-        sumIntensity = sum(currentaoi(:));
+        % Assign the result to tempData
+        sumIntensity = sum(currentAoiImage(:));
         % Shift the x, y coordinates from origin in AOI to origin at image
-        outarg(2:3) = outarg(2:3) + aoi_origin';
-        ImageDataParallel(aoiindx2, 1, framemapindx) = aoiindx2;
-        ImageDataParallel(aoiindx2, 2, framemapindx) = currentFrameNumber;
-        ImageDataParallel(aoiindx2, 3:7, framemapindx) = outarg(:);
-        ImageDataParallel(aoiindx2, 8, framemapindx) = sumIntensity;
-        
+        fittedCoeff(2:3) = fittedCoeff(2:3) + aoi_origin';
+        tempData(iAOI, 1, iFrame) = iAOI;
+        tempData(iAOI, 2, iFrame) = currentFrameNumber;
+        tempData(iAOI, 3:7, iFrame) = fittedCoeff(:);
+        tempData(iAOI, 8, iFrame) = sumIntensity;
     end
 end
 
 % Pre-Allocate space
-pc = zeros(nAOI*nFrame, 8);
+data = zeros(nAOI*nFrame, 8);
 
 % ImageDataParallel(aoiindx,DataEntryIndx,FrmIndx)
 % Reshaping data matrices for output
@@ -59,7 +99,7 @@ pc = zeros(nAOI*nFrame, 8);
 % satisfy the parallel processing loop requirements for indexing
 
 for frameindex=1:nFrame
-    pc((frameindex-1)*nAOI+1:(frameindex-1)*nAOI+nAOI,:)=ImageDataParallel(:,:,frameindex);
+    data((frameindex-1)*nAOI+1:(frameindex-1)*nAOI+nAOI,:)=tempData(:,:,frameindex);
 end
 
 end
