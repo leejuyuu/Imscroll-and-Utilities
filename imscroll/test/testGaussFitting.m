370b9b2df96ce2130e2218d784ff4d617e8ca7a0
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-02 16:47:33 +0800

    Delete the old test case to avoid confusion
    The two test case are exactly the same, so delete this one that uses the
    handles structure.

--- imscroll/test/testGaussFitting.m
+++ imscroll/test/testGaussFitting.m
@@ -2,15 +2,6 @@ function tests = testGaussFitting
 tests = functiontests(localfunctions);
 end
 
-function testFirst(testCase)
-load('imscroll/test/test_data/gui_state.mat')
-FitAOIs_Callback_outer(hObject, eventdata, handles)
-new_ans = load('imscroll/test/test_data/default.dat', '-mat');
-right_ans = load('imscroll/test/test_data/L2_driftfit_20f_track_avg1.dat', '-mat');
-verifyEqual(testCase, new_ans.aoifits.data, right_ans.aoifits.data)
-close all
-end
-
 function testFitAoisTo2dGaussian(testCase)
 % This only tests for tracking is on
 loaded = load('imscroll/test/test_data/L2_gauss_test_param.mat');
