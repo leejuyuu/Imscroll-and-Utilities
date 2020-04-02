fa7f9df9d2be1867e5f5aceeec87043f376a9462
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-02 21:01:55 +0800

    Refine the mappingfit_fminsearch function
    - Use a shorter fminsearch calling.
    - Move the mappingfunc into this file

--- imscroll/mapping/mappingfit_fminsearch.m
+++ imscroll/mapping/mappingfit_fminsearch.m
@@ -35,4 +35,28 @@ function pc=mappingfit_fminsearch(indata,startCoeff)
 % You should have received a copy of the GNU General Public License
 % along with this software. If not, see <http://www.gnu.org/licenses/>.
 
-pc=fminsearch('mappingfunc_fminsearch',startCoeff(:),[],indata{1},indata{2});
+fun = @(coeff) mappingfunc_fminsearch(coeff, indata{1}, indata{2});
+pc = fminsearch(fun, startCoeff(:));
+
+function pc=mappingfunc_fminsearch(inarg,xdata,ydata)
+%
+% function mappingfunc_fminsearch(inarg,xdata,ydata
+%
+% This function will be called from mappingfit_fminsearch() while using the
+% lscurvefit() function to map the x1y1 image1 point pairs onto x2 or y2
+% points.  
+% The form of the mapping is
+% x2 = mxx21 * x1 + mxy21* y1 + bx
+% or
+% y2 = myx21 * x1 + myy21 * y1 + by
+%
+% where the input arguements are given in inarg according to
+% inarg  == [ mxx21 mxy21 bx21] (mapping to x2) or [ myx21 myy21 by] (when
+%                                                          mapping to y2)
+% xdata == n x 2 list of [x1(:) y1(:)] points that will be mapped onto
+%          either an x2(:) or y2(:) list of output points
+% ydata== 1 x n list of either x2(:) or y2(:) output points
+% see also mappingfunc_rot_mag_trans.m for fit to a transformation that is
+% strictly a translation, magnification and rotation (4 free parameters)
+
+pc= sum( (ydata -(inarg(1)*xdata(:,1) + inarg(2)*xdata(:,2) + inarg(3)) ).^2);
