21922eb0c8b4ea1224aa24ba08da1b0eeb7ddec5
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-02 21:49:29 +0800

    Separate the part that does plotting from fitting

--- imscroll/mapping/MakeMappingFile.m
+++ imscroll/mapping/MakeMappingFile.m
@@ -52,18 +52,7 @@ if nAOIs>=3               % Map the two fields if we have 3 or more points
     % mxy21 = 0 at first
     %****fitparmx21more=mappingfit(inarray,[fitparmx21(1) 0 fitparmx21(2) ]);
     fitparmx21more=mappingfit_fminsearch([x1, y1], x2,[fitparmx21(1) 0 fitparmx21(2) ]);
-    rangex=[min(x1): max(x1)];
-    % Get the linear fit result
-    valx=polyval(fitparmx21,rangex);
-    % Get the more complex fit result
-    valxmore=mappingfunc(fitparmx21more,[x1, y1]);
-    % Plot the x data and fit
-    figure(20);subplot(121);plot(x1,x2,'o',...
-        rangex,valx,'r-', x1,valxmore,'x')
-    xlabel('X1 Coordinate');ylabel('X2 Coordinate');title(['X Mapping:' num2str(fitparmx21more')])
-    % look at the diff btwn the input x2 and
-    % mapped x2 (not prox mapped =>systematic deviations)
-    %figure(21);plot(mappingpoints(:,3),mappingpoints(:,9)-valxmore,'o');
+    
     % then polyfit(y1,y2)
     fitparmy21=polyfit(y1, y2, 1);
     
@@ -72,36 +61,64 @@ if nAOIs>=3               % Map the two fields if we have 3 or more points
     % myx21 = 0 at first
     %***fitparmy21more=mappingfit(inarray,[0 fitparmx21(1) fitparmx21(2) ]);
     fitparmy21more=mappingfit_fminsearch([x1, y1], y2,[0 fitparmx21(1) fitparmx21(2) ]);
-    rangey=[min(y1): max(y1)];
-    % Get the linear fit result
-    valy=polyval(fitparmy21,rangey);
-    % Get the more complex fit result
-    valymore=mappingfunc(fitparmy21more,[x1, y1]);
-    % Plot the y data and fit
-    
-    figure(20);subplot(122);plot(y1,y2,'o',...
-        rangey,valy,'-',y1,valymore,'x')
-    xlabel('Y1 Coordinate');ylabel('Y2 Coordinate');title(['Y Mapping:' num2str(fitparmy21more')])
-    % look at the diff btwn the input y2 and mapped y2
-    % (not prox mapped => systematic deviations
-    %figure(22);plot(mappingpoints(:,4),mappingpoints(:,10)-valymore,'o');
     
     % Place fitparm into 'Value' of FitDisplay
     % as a two row matrix
     
     fitparmvector=[fitparmx21more';fitparmy21more'];
+    fitparmvectorLess = [fitparmx21';fitparmy21'];
     
     % display as row [mxx21 mxy21 bx21 myx21 myy21 by21]'
     
     %save p:\matlab12\larry\fig-files\imscroll\mapping\fitparms.dat fitparmvector mappingpoints
     eval(['save ' filename ' fitparmvector mappingpoints']);
-    mappingpoints
+    mappingpoints;
 else
     sprintf('aoiinfo2 matrix must contain at least 3 aois')
     
 end
 pc.fitparmvector=fitparmvector;
 pc.mappingpoints=mappingpoints;
+
+plotMappingResult(pc, fitparmvectorLess)
+end
+
+function plotMappingResult(pc, fitparmvectorLess)
+
+x1 = pc.mappingpoints(:, 3);
+y1 = pc.mappingpoints(:, 4);
+x2 = pc.mappingpoints(:, 9);
+y2 = pc.mappingpoints(:, 10);
+
+rangex=[min(x1): max(x1)];
+    % Get the linear fit result
+    valx=polyval(fitparmvectorLess(1, :),rangex);
+    % Get the more complex fit result
+    valxmore=mappingfunc(pc.fitparmvector(1, :),[x1, y1]);
+    % Plot the x data and fit
+    figure(20);subplot(121);plot(x1,x2,'o',...
+        rangex,valx,'r-', x1,valxmore,'x')
+    xlabel('X1 Coordinate');ylabel('X2 Coordinate');title(['X Mapping:' num2str(pc.fitparmvector(1, :))])
+    % look at the diff btwn the input x2 and
+    % mapped x2 (not prox mapped =>systematic deviations)
+    %figure(21);plot(mappingpoints(:,3),mappingpoints(:,9)-valxmore,'o');
+    
+    rangey=[min(y1): max(y1)];
+    % Get the linear fit result
+    valy=polyval(fitparmvectorLess(2, :),rangey);
+    % Get the more complex fit result
+    valymore=mappingfunc(pc.fitparmvector(2, :),[x1, y1]);
+    % Plot the y data and fit
+    
+    figure(20);subplot(122);plot(y1,y2,'o',...
+        rangey,valy,'-',y1,valymore,'x')
+    xlabel('Y1 Coordinate');ylabel('Y2 Coordinate');title(['Y Mapping:' num2str(pc.fitparmvector(2, :))])
+    % look at the diff btwn the input y2 and mapped y2
+    % (not prox mapped => systematic deviations
+    %figure(22);plot(mappingpoints(:,4),mappingpoints(:,10)-valymore,'o');
+    
+
+
 end
 
 
