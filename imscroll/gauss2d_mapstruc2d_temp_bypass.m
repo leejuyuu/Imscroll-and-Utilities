ee7cb40ae42f912714a6b831ca94d41398c7c4fc
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-02 12:33:53 +0800

    Extract the gaussian fitting code into function

--- imscroll/gauss2d_mapstruc2d_temp_bypass.m
+++ imscroll/gauss2d_mapstruc2d_temp_bypass.m
@@ -53,69 +53,14 @@ elseif fitChoice == 1
         error('background choice is not supported in this version')
     end
     aoiinfo = parenthandles.FitData;
+    pc = fitAoisTo2dGaussian(...
+        aoiinfo,...
+        aoiProcessParameters,...
+        imageFileProperty,...
+        isTrackAOI...
+        );
     
-    nAOI = length(aoiinfo(:, 1));
-    nFrame = length(aoiProcessParameters.frameRange);
     
-    % Pre-Allocate space
-    ImageDataParallel = zeros(nAOI, 8, nFrame);
-    BackgroundDataParallel = zeros(nAOI, 8, nFrame);
-    
-    frameAverage = aoiProcessParameters.frameAverage;
-    
-    %Now loop through the remaining frames
-    for framemapindx=1:nFrame
-        if framemapindx/10==round(framemapindx/10)
-            framemapindx
-        end
-        % Get the averaged image of this frame to process
-        currentFrameNumber = aoiProcessParameters.frameRange(framemapindx);
-        
-        currentfrm = getAveragedImage(...
-            imageFileProperty,...
-            currentFrameNumber,...
-            frameAverage);
-        for aoiindx2=1:nAOI   % Loop through all the aois for this frame
-            
-            if isTrackAOI && framemapindx ~= 1 && framemapindx ~= 2
-                coord = ImageDataParallel(aoiindx2,4:5,framemapindx - 1);
-            else
-                coord = aoiinfo(aoiindx2, 3:4);
-            end
-            
-            pixnum = aoiinfo(aoiindx2, 5);
-            [currentaoi, aoi_origin] = getAOIsubImageAndCenterDuplicate(currentfrm, coord, pixnum/2);
-
-            inputarg0 = guessStartingParameters(double(currentaoi));
-            
-            % Now fit the current aoi
-            outarg=gauss2dfit(double(currentaoi),double(inputarg0));            
-            
-            % ImageDataParallel: [aoi#     frm#       amp    xo    yo    sigma  offset (int inten)]
-            sumIntensity = sum(currentaoi(:));
-            % Shift the x, y coordinates from origin in AOI to origin at image
-            outarg(2:3) = outarg(2:3) + aoi_origin';
-            ImageDataParallel(aoiindx2, 1, framemapindx) = aoiindx2;
-            ImageDataParallel(aoiindx2, 2, framemapindx) = currentFrameNumber;
-            ImageDataParallel(aoiindx2, 3:7, framemapindx) = outarg(:);
-            ImageDataParallel(aoiindx2, 8, framemapindx) = sumIntensity;
-            
-        end
-    end
-    
-    % Pre-Allocate space
-    pc.ImageData = zeros(nAOI*nFrame, 8);
-    pc.BackgroundData = zeros(nAOI*nFrame, 8);
-    
-    % ImageDataParallel(aoiindx,DataEntryIndx,FrmIndx)
-    % Reshaping data matrices for output
-    % The form ImageData and BackgroundData matrices was required to
-    % satisfy the parallel processing loop requirements for indexing
-    
-    for frameindex=1:nFrame
-        pc.ImageData((frameindex-1)*nAOI+1:(frameindex-1)*nAOI+nAOI,:)=ImageDataParallel(:,:,frameindex);
-        pc.BackgroundData((frameindex-1)*nAOI+1:(frameindex-1)*nAOI+nAOI,:)=BackgroundDataParallel(:,:,frameindex);
-    end
 else
     
     error('the chosen fitting method isn''t supported in this version')
@@ -145,3 +90,71 @@ startParams = [...
     ];
 end
 
+function pc = fitAoisTo2dGaussian(aoiinfo,...
+                                  aoiProcessParameters,...
+                                  imageFileProperty,...
+                                  isTrackAOI)
+nAOI = length(aoiinfo(:, 1));
+nFrame = length(aoiProcessParameters.frameRange);
+
+% Pre-Allocate space
+ImageDataParallel = zeros(nAOI, 8, nFrame);
+BackgroundDataParallel = zeros(nAOI, 8, nFrame);
+
+frameAverage = aoiProcessParameters.frameAverage;
+
+%Now loop through the remaining frames
+for framemapindx=1:nFrame
+    if framemapindx/10==round(framemapindx/10)
+        framemapindx
+    end
+    % Get the averaged image of this frame to process
+    currentFrameNumber = aoiProcessParameters.frameRange(framemapindx);
+    
+    currentfrm = getAveragedImage(...
+        imageFileProperty,...
+        currentFrameNumber,...
+        frameAverage);
+    for aoiindx2=1:nAOI   % Loop through all the aois for this frame
+        
+        if isTrackAOI && framemapindx ~= 1 && framemapindx ~= 2
+            coord = ImageDataParallel(aoiindx2,4:5,framemapindx - 1);
+        else
+            coord = aoiinfo(aoiindx2, 3:4);
+        end
+        
+        pixnum = aoiinfo(aoiindx2, 5);
+        [currentaoi, aoi_origin] = getAOIsubImageAndCenterDuplicate(currentfrm, coord, pixnum/2);
+        
+        inputarg0 = guessStartingParameters(double(currentaoi));
+        
+        % Now fit the current aoi
+        outarg=gauss2dfit(double(currentaoi),double(inputarg0));
+        
+        % ImageDataParallel: [aoi#     frm#       amp    xo    yo    sigma  offset (int inten)]
+        sumIntensity = sum(currentaoi(:));
+        % Shift the x, y coordinates from origin in AOI to origin at image
+        outarg(2:3) = outarg(2:3) + aoi_origin';
+        ImageDataParallel(aoiindx2, 1, framemapindx) = aoiindx2;
+        ImageDataParallel(aoiindx2, 2, framemapindx) = currentFrameNumber;
+        ImageDataParallel(aoiindx2, 3:7, framemapindx) = outarg(:);
+        ImageDataParallel(aoiindx2, 8, framemapindx) = sumIntensity;
+        
+    end
+end
+
+% Pre-Allocate space
+pc.ImageData = zeros(nAOI*nFrame, 8);
+pc.BackgroundData = zeros(nAOI*nFrame, 8);
+
+% ImageDataParallel(aoiindx,DataEntryIndx,FrmIndx)
+% Reshaping data matrices for output
+% The form ImageData and BackgroundData matrices was required to
+% satisfy the parallel processing loop requirements for indexing
+
+for frameindex=1:nFrame
+    pc.ImageData((frameindex-1)*nAOI+1:(frameindex-1)*nAOI+nAOI,:)=ImageDataParallel(:,:,frameindex);
+    pc.BackgroundData((frameindex-1)*nAOI+1:(frameindex-1)*nAOI+nAOI,:)=BackgroundDataParallel(:,:,frameindex);
+end
+
+end
