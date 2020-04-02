1cd7157536341c5556212a9dd4d06ef2c0e3b6bd
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-02 20:25:49 +0800

    Rename variables to facilitate reading
    Rename the variables in MakeMappingFile. Replace cryptic indexing with
    names to facilitate reading.

--- imscroll/mapping/MakeMappingFile.m
+++ imscroll/mapping/MakeMappingFile.m
@@ -30,63 +30,66 @@ function pc=MakeMappingFile(aoiinfo2_field1, aoiinfo2_field2, filename)
 % along with this software. If not, see <http://www.gnu.org/licenses/>.
 
 % Define the mappingpoints matrix
-[rose col]=size(aoiinfo2_field1);
+[nAOIs, ~]=size(aoiinfo2_field1);
 aoiinfo2_field1=update_FitData_aoinum(aoiinfo2_field1);       % Update the numbering of the AOIs in the map
 aoiinfo2_field2=update_FitData_aoinum(aoiinfo2_field2);
 mappingpoints=[aoiinfo2_field1 aoiinfo2_field2];    % There will be an error if the two aoiinfo2
 % are not the same size
 % Now we must obtain the fitparmsvector
+x1 = aoiinfo2_field1(:, 3);
+y1 = aoiinfo2_field1(:, 4);
+x2 = aoiinfo2_field2(:, 3);
+y2 = aoiinfo2_field2(:, 4);
 
-
-if rose>=3               % Map the two fields if we have 3 or more points
+if nAOIs>=3               % Map the two fields if we have 3 or more points
     %  x2= mx21*x1 + bx21
     %  y2= my21*y1 + by21
     % first polyfit(x1,x2)=[slope intercept] as
     % first guess
-    fitparmx21=polyfit(mappingpoints(:,3),mappingpoints(:,9),1);
+    fitparmx21=polyfit(x1, x2, 1);
     % Form a cell array, first member is a matrix of the
     % x1y1 pairs
-    inarray{1}=[ mappingpoints(:,3) mappingpoints(:,4)];
+    inarray{1}=[ x1 y1];
     % second member is a vector of the output
     % x2 points
-    inarray{2} = mappingpoints(:,9);
+    inarray{2} = x2;
     % Input guess is [mxx21 mxy21 bx] with
     % mxy21 = 0 at first
     %****fitparmx21more=mappingfit(inarray,[fitparmx21(1) 0 fitparmx21(2) ]);
     fitparmx21more=mappingfit_fminsearch(inarray,[fitparmx21(1) 0 fitparmx21(2) ]);
-    rangex=[min(mappingpoints(:,3) ): max(mappingpoints(:,3) )];
+    rangex=[min(x1): max(x1)];
     % Get the linear fit result
     valx=polyval(fitparmx21,rangex);
     % Get the more complex fit result
     valxmore=mappingfunc(fitparmx21more,inarray{1});
     % Plot the x data and fit
-    figure(20);subplot(121);plot(mappingpoints(:,3),mappingpoints(:,9),'o',...
-        rangex,valx,'r-', mappingpoints(:,3),valxmore,'x')
+    figure(20);subplot(121);plot(x1,x2,'o',...
+        rangex,valx,'r-', x1,valxmore,'x')
     xlabel('X1 Coordinate');ylabel('X2 Coordinate');title(['X Mapping:' num2str(fitparmx21more')])
     % look at the diff btwn the input x2 and
     % mapped x2 (not prox mapped =>systematic deviations)
     %figure(21);plot(mappingpoints(:,3),mappingpoints(:,9)-valxmore,'o');
     % then polyfit(y1,y2)
-    fitparmy21=polyfit(mappingpoints(:,4),mappingpoints(:,10),1);
+    fitparmy21=polyfit(y1, y2, 1);
     % Form a cell array, first member is a
     % matrix of the x1y1 pairs
-    inarray{1}=[ mappingpoints(:,3) mappingpoints(:,4)];
+    inarray{1}=[ x1 y1];
     % second member is a vector of the output
     % y2 points
-    inarray{2} = mappingpoints(:,10);
+    inarray{2} = y2;
     % Input guess is [myx21 myy21 bx] with
     % myx21 = 0 at first
     %***fitparmy21more=mappingfit(inarray,[0 fitparmx21(1) fitparmx21(2) ]);
     fitparmy21more=mappingfit_fminsearch(inarray,[0 fitparmx21(1) fitparmx21(2) ]);
-    rangey=[min(mappingpoints(:,4) ): max(mappingpoints(:,4) )];
+    rangey=[min(y1): max(y1)];
     % Get the linear fit result
     valy=polyval(fitparmy21,rangey);
     % Get the more complex fit result
     valymore=mappingfunc(fitparmy21more,inarray{1});
     % Plot the y data and fit
     
-    figure(20);subplot(122);plot(mappingpoints(:,4),mappingpoints(:,10),'o',...
-        rangey,valy,'-',mappingpoints(:,4),valymore,'x')
+    figure(20);subplot(122);plot(y1,y2,'o',...
+        rangey,valy,'-',y1,valymore,'x')
     xlabel('Y1 Coordinate');ylabel('Y2 Coordinate');title(['Y Mapping:' num2str(fitparmy21more')])
     % look at the diff btwn the input y2 and mapped y2
     % (not prox mapped => systematic deviations
