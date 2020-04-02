19f104cff2fe5ed80bc861100a6e17ece3ccd36c
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-02 16:45:30 +0800

    Add a test case to test the extracted function
    Add a test case using the same data, without the handles structure, to
    test the fitAoisTo2dGaussian function. Only the tracking case is covered.
    
    Include the test data in this commit.

--- imscroll/test/testGaussFitting.m
+++ imscroll/test/testGaussFitting.m
@@ -5,8 +5,28 @@ end
 function testFirst(testCase)
 load('imscroll/test/test_data/gui_state.mat')
 FitAOIs_Callback_outer(hObject, eventdata, handles)
-new_ans = load('imscroll/test/test_data/default.dat', '-mat')
-right_ans = load('imscroll/test/test_data/L2_driftfit_20f_track_avg1.dat', '-mat')
+new_ans = load('imscroll/test/test_data/default.dat', '-mat');
+right_ans = load('imscroll/test/test_data/L2_driftfit_20f_track_avg1.dat', '-mat');
 verifyEqual(testCase, new_ans.aoifits.data, right_ans.aoifits.data)
 close all
+end
+
+function testFitAoisTo2dGaussian(testCase)
+% This only tests for tracking is on
+loaded = load('imscroll/test/test_data/L2_gauss_test_param.mat');
+aoiProcessParameters = struct(...
+    'frameAverage', 1,...
+    'frameRange', 1:20 ...
+);
+isTrackAOI = true;
+
+data = fitAoisTo2dGaussian(...
+        loaded.aoiinfo,...
+        aoiProcessParameters,...
+        loaded.imageFileProperty,...
+        isTrackAOI...
+        );
+right_ans = load('imscroll/test/test_data/L2_driftfit_20f_track_avg1.dat', '-mat');
+verifyEqual(testCase, data, right_ans.aoifits.data)
+
 end
