6528dbf2c09b9cc3b7a0f905042fd6fc8dae5e9c
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-04 00:48:29 +0800

    Add test for Remove_Close_AOIs_v1
    Test against dummy aois with constructed distances.

---
+++ imscroll/test/testAoiOperation.m
@@ -0,0 +1,54 @@
+function tests = testAoiOperation
+tests = functiontests(localfunctions);
+end
+
+function testRemoveCloseAOIs(testCase)
+% Case 1: x spacing
+aoiinfo = ones(7, 6);
+aoiinfo(:, 3) = [1, 3, 4, 6, 8, 9, 11]' * 4;
+new_aoiinfo = Remove_Close_AOIs_v1(aoiinfo, 5);
+% verifyEqual(testCase, new_aoiinfo, aoiinfo([1, 4, 7], :))
+a = aoiinfo([1, 4, 7], :);
+a(:, 6) = 1:3;
+verifyEqual(testCase, new_aoiinfo, a);
+
+
+% Case 2: y spacing
+aoiinfo = ones(7, 6);
+aoiinfo(:, 4) = [1, 3, 4, 6, 8, 9, 11]' * 4;
+new_aoiinfo = Remove_Close_AOIs_v1(aoiinfo, 5);
+% verifyEqual(testCase, new_aoiinfo, aoiinfo([1, 4, 7], :))
+a = aoiinfo([1, 4, 7], :);
+a(:, 6) = 1:3;
+verifyEqual(testCase, new_aoiinfo, a);
+
+% Case 3: Boundary
+aoiinfo = ones(4, 6);
+aoiinfo(:, 3) = [1, 3, 4, 8]' * 5;
+new_aoiinfo = Remove_Close_AOIs_v1(aoiinfo, 5);
+a = aoiinfo([1, 4], :);
+a(:, 6) = 1:2;
+verifyEqual(testCase, new_aoiinfo, a);
+
+% Case 4: Not on axis
+aoiinfo = ones(7, 6);
+aoiinfo(:, 3) = [1, 3, 4, 6, 8, 9, 11]' * 4 * cos(pi/3);
+aoiinfo(:, 4) = [1, 3, 4, 6, 8, 9, 11]' * 4 * sin(pi/3);
+new_aoiinfo = Remove_Close_AOIs_v1(aoiinfo, 5);
+% verifyEqual(testCase, new_aoiinfo, aoiinfo([1, 4, 7], :))
+a = aoiinfo([1, 4, 7], :);
+a(:, 6) = 1:3;
+verifyEqual(testCase, new_aoiinfo, a);
+
+
+% Case 5: aggregates
+aoiinfo = ones(7, 6);
+aoiinfo(:, 3) = [1, 1, 2, 6, 11, 13, 15]' ;
+aoiinfo(:, 4) = [1, 2, 1, 6, 11, 13, 14]' ;
+new_aoiinfo = Remove_Close_AOIs_v1(aoiinfo, 5);
+% verifyEqual(testCase, new_aoiinfo, aoiinfo([1, 4, 7], :))
+a = aoiinfo(4, :);
+verifyEqual(testCase, new_aoiinfo, a);
+
+
+end
