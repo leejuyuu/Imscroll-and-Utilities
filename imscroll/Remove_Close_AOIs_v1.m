6ba06714adfc6b933d2fbe178405cbe133233798
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-04 01:35:53 +0800

    Refactor Remove_Close_AOIs_v1
    Refactor the function to be simpler and more readable. Improve the doc.

--- imscroll/Remove_Close_AOIs_v1.m
+++ imscroll/Remove_Close_AOIs_v1.m
@@ -1,21 +1,27 @@
-function pc=Remove_Close_AOIs_v1(aoiinfo2,Unique_Landing_Radius)
+function new_aoiinfo2 = Remove_Close_AOIs_v1(aoiinfo2, radius)
 %
-% pc=Remove_Close_AOIs_v1(xytot,Unique_Landing_Radius)
+% new_aoiinfo2 = Remove_Close_AOIs_v1(aoiinfo2, radius)
 % 
-% This function will output an
-% aoiinfo2 structure containing a list of aois located at the landing sites.  
-% specified in the xytot list.  Those aois (landings) are culled so that no 
-% two aois will be closer than a distance specified by UniqueLandingRadius 
+% Remove AOIs that are too close to any other AOI in the given 'aoiinfo2'
+% array, and return a new, mutually far away enough 'new_aoiinfo2' array.
+% Too close here means that the distance between 2 AOIs is smaller than or
+% equal to the 'radius' argument. So no two aois will be closer than a 
+% distance specified by 'radius'.
+% 
+% Input args:
+%     aoiinfo2: N x 6 array, the handles.FitData array, with each row being
+%               data for each AOI, and has the column structure as follows.
+%               [frame#, frameAverage, xCoord, yCoord, aoiWidth, aoi#]
 %
-% aoiinfo2 == output of aoi location information from imscroll
-%           [ frm#   ave#   x   y   pixnum   aoi#]
-% UniqueLandingRadius == the list of output aois in aoiinfo2 will be culled 
-%                       so that we eliminate those aois from the list that
-%                       are closer that a distance 'UniqueLandingRadius'
-%                       from any other aoi.  That is, we remove aois that
-%                       are clustered together, retaining only those aois
-%                       that are well isolated from others. (units: pixels)
-
+%     radius: float (unit: pixel), any AOI that has one or more other AOIs 
+%             present in the circular region specified by the radius and the 
+%             coordinate of this AOI, will be removed.
+%
+% Output arg:
+%     new_aoiinfo2: M x 6 array, the aoiinfo2 array with AOIs that are
+%                   clustered to each other removed. (M <= N)
+ 
+% Copyright 2020 Tzu-Yu Lee, National Taiwan University.
 % Copyright 2015 Larry Friedman, Brandeis University.
 
 % This is free software: you can redistribute it and/or modify it under the
@@ -29,39 +35,21 @@ function pc=Remove_Close_AOIs_v1(aoiinfo2,Unique_Landing_Radius)
 % You should have received a copy of the GNU General Public License
 % along with this software. If not, see <http://www.gnu.org/licenses/>.
 
-frm=aoiinfo2(1,1);
-ave=aoiinfo2(1,2);      % Pick off frame, average and pixnum
-pixnum=aoiinfo2(1,5);
-xytot=aoiinfo2(:,3:4);                  % [ x y ] list of aoi centers
+xyCoords = aoiinfo2(:, 3:4);
+nAOIs = length(aoiinfo2(:, 1));
 
-[landingcount colmn]=size(xytot);       % landingcount = starting # of aois in list
-% colmn = 2
-xytotaccum=zeros(landingcount,2);       % [x y] list that we will be outputing
-% of max length of landingcount
-ListIndx=1;
-for indx=1:landingcount
+isFarFromOthers = false(nAOIs, 1);
+for iAOI = 1:nAOIs
     % Test each aoi against all the others
-    tstaoi=xytot(indx,:);           % AOI under test
-    dtest=sqrt( (xytot(:,1)-tstaoi(1,1)).^2 + (xytot(:,2)-tstaoi(1,2)).^2 );
-    logik=dtest~=0;             % Pick all AOIs other than the one under test
-    xytotdum=xytot(logik,:);
+    thisAoiCoord=xyCoords(iAOI,:);
+    allOtherAoiCoords = xyCoords;
+    allOtherAoiCoords(iAOI, :) = [];
     % Distance of test aoi to all others
-    dtest=sqrt( (xytotdum(:,1)-tstaoi(1,1)).^2 + (xytotdum(:,2)-tstaoi(1,2)).^2 );
-    logik=dtest>(Unique_Landing_Radius);     % Keep only aois that are not too close together
-    if sum(logik)==landingcount-1
-        % Here if aoi under test is not close to any others
-        xytotaccum(ListIndx,:)=tstaoi;  % In which case we keep the current tstaoi
-        % and add it to the output list
-        ListIndx=ListIndx+1;
-    end
+    distance = sqrt( (allOtherAoiCoords(:,1)-thisAoiCoord(1)).^2 +...
+        (allOtherAoiCoords(:,2)-thisAoiCoord(2)).^2 );
+    isFarFromOthers(iAOI) = all(distance > radius);
 end
 
-
-% Now eliminate all the empty entries in the output list
-logik=(xytotaccum(:,1)~=0)&(xytotaccum(:,2)~=0);
-xytotaccum=xytotaccum(logik,:);         % Keep only nonzero elements (zero from initialization
-[rose colm]=size(xytotaccum);
-% Construct the output aoiinfo2 list
-pc=[ones(rose,1)*frm  ones(rose,1)*ave    xytotaccum  ones(rose,1)*pixnum  [1:rose]' ];
-
+new_aoiinfo2 = aoiinfo2(isFarFromOthers, :);
+new_aoiinfo2(:, 6) = 1:length(new_aoiinfo2(:, 6));
 
