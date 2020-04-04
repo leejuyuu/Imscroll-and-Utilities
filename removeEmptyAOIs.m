d1c4941f4d90cd5d3694e6efdbe0b3e253a79530
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-04 16:19:28 +0800

    Refactor clean up removeEmptyAOIs

--- removeEmptyAOIs.m
+++ removeEmptyAOIs.m
@@ -1,44 +1,23 @@
-function new_aoiinfo2 = removeEmptyAOIs(aoiinfo2, AllSpots, radius)
-% Here to remove AOIs that do not contain a
-% spot  case12
-% Spots within radius distance
+function new_aoiinfo2 = removeEmptyAOIs(aoiinfo2, AllSpots, radius, choice)
+% Remove AOIs from 'aoiinfo2' that do not contain a spot in 'AllSpots' within a 
+% circular region defined by 'radius'.
 
-
-% Remove AOIs that do not contain a detected spot
-% Now the AllSpots structure
-% contains a list of all spots in the current
-% frame.  We next want to remove all the current
-% AOIs that do not contain one of these spots
-
-     % Use as max pixel distance to a spot.
-% A spot must be this close to an AOI center
-% for that AOI to  be retained
-radius_hys = 1;     % A multiplicative constant not used here
-
-% [framenumber ave x y pixnum aoinumber];
+% A multiplicative constant not important here
+radius_hys = 1;
 
 nAOIs = length(aoiinfo2(:, 1));
-AOIspots = zeros(nAOIs,2);     % We will denote the AOI spot number N
-% as containing a spot by marking
-% AOIspots(N,2) = 1
 
 % This third index is for the frame number.
 xyCoord = zeros(nAOIs, 2, 1);
 xyCoord(:, :, 1) = aoiinfo2(:, 3:4);
+
+AOIspots = zeros(nAOIs,2);  % [frameNumber, containSpot(0 or 1)];
 for iAOI = 1:nAOIs
-    
-    % Cycle through all the aois
     AOIspots(iAOI,:)=AOISpotLanding2(aoiinfo2(iAOI,6),radius,radius_hys, AllSpots, ...
         AllSpots, xyCoord);
-    
 end
-% We have now found all the AOIs w/ and w/o spots and need
-% to remove those AOIs without spots
-% Keep only those rows i for which AOIspots(i,2) = 1
+
+% Remove those AOIs that does not contains spot.
 new_aoiinfo2 = aoiinfo2(logical(AOIspots(:,2)),:);
 new_aoiinfo2 = update_FitData_aoinum(new_aoiinfo2);
-
-
-
-
 end
