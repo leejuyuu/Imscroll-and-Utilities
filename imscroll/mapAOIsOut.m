9f08e57815c9e3b4df0e7ca0977701f0254b515b
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-05 13:29:59 +0800

    Clean up mapAOIsOut

--- imscroll/mapAOIsOut.m
+++ imscroll/mapAOIsOut.m
@@ -5,29 +5,26 @@ function mapAOIsOut(handles)
 % (output is x2y2  coordinates)
 % Save the current AOI locations before mapping
 aoiinfo2=handles.FitData;
-eval(['save ' handles.dataDir.String 'premapAOIs.dat' ' aoiinfo2']);
+save([handles.dataDir.String, 'premapAOIs.dat'], 'aoiinfo2');
 fitparmvector=get(handles.FitDisplay,'UserData');     % Fetch the mapping parameters
 % Stored as [mxx21 mxy21 bx;
 %            myx21 myy21 by]
 % handles.Fitdata=[ frm# ave AOIx  AOIy pixnum aoinum]
 
-nowpts=[handles.FitData(:,3) handles.FitData(:,4)];
+currentCoords = handles.FitData(:, 3:4);
 % Now map to the x2
 
-handles.FitData(:,3)=mappingfunc(fitparmvector(1,:),nowpts);
-%handles.FitData(:,3)=handles.FitData(:,3)*fitparmvector(1) + fitparmvector(2);
-% Now map to the y2
-handles.FitData(:,4)=mappingfunc(fitparmvector(2,:),nowpts);
-%handles.FitData(:,4)=handles.FitData(:,4)*fitparmvector(3) + fitparmvector(4);
+handles.FitData(:, 3:4) = mapXY(currentCoords, fitparmvector);
+
 if get(handles.ProximityMappingToggle,'Value')==1
     % Here to instead use proximity mapping method
-    [rosenow colnow]=size(nowpts);
-    mappednow=zeros(rosenow,2);
-    for indx=1:rosenow
+    [nAOIs, ~] = size(currentCoords);
+    proxMappedCoords = zeros(nAOIs,2);
+    for iAOI = 1:nAOIs
         % Use 15 nearest points for mapping to field2
-        mappednow(indx,:)=proximity_mapping_v1(handles.MappingPoints,nowpts(indx,:),15,fitparmvector,2);
+        proxMappedCoords(iAOI,:)=proximity_mapping_v1(handles.MappingPoints,currentCoords(iAOI,:),15,fitparmvector,2);
     end
-    handles.FitData(:,3:4)=mappednow;
+    handles.FitData(:,3:4) = proxMappedCoords;
 end
 
 
@@ -38,7 +35,7 @@ handles.FitData=update_FitData_aoinum(handles.FitData);
 %     guidata(gcbo,handles)
 % Save the current AOI locations after mapping
 aoiinfo2=handles.FitData;
-eval(['save ' handles.dataDir.String 'postmapAOIs.dat aoiinfo2']);
+save([handles.dataDir.String, 'postmapAOIs.dat'], 'aoiinfo2');
 set(handles.ButtonChoice,'Value',7);           % Make ButtonChoice menu ready to reload mapped AOIs
 set(handles.InputParms,'String','postmapAOIs.dat')     % Put name of postmap AOI file into editable text field (ready to re-load)
 
