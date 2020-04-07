5b0a633615a7f17233d51bf83e884d45c67f173e
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-08 01:20:58 +0800

    Add tests for ShiftAOIs
    I want to start probing its behaviors and refactor this function.

--- imscroll/test/testDriftList.m
+++ imscroll/test/testDriftList.m
@@ -20,3 +20,58 @@ verifyEqual(testCase, drifts.diffdriftlist, correct_driftlist);
 verifyEqual(testCase, driftlist, correct_driftlist);
 
 end
+
+function testShiftAOIs(testCase)
+driftlist = ones(10, 3);
+driftlist(1, 2:3) = 0;
+driftlist(:, 1) = 1:10;
+
+% Test normal case: AOIs are defined in the first frame
+aoiinfo = [1, 1, 0, 0, 5, 1];
+for i = 1:10
+xy = ShiftAOI(1, i, aoiinfo, driftlist);
+verifyEqual(testCase, xy, [i-1, i-1]);
+end
+
+% Test if AOIs are defined in the middle of the sequence
+aoiinfo = [5, 1, 0, 0, 5, 1];
+for i = 1:10
+xy = ShiftAOI(1, i, aoiinfo, driftlist);
+verifyEqual(testCase, xy, [i-5, i-5]);
+end
+
+% Test if changing frame average affect the result
+aoiinfo = [5, 5, 0, 0, 5, 1];
+for i = 1:10
+xy = ShiftAOI(1, i, aoiinfo, driftlist);
+verifyEqual(testCase, xy, [i-5, i-5]);
+end
+
+% Test for defined AOI is outside the driftlist frame range
+aoiinfo = [11, 5, 0, 0, 5, 1];
+for i = 1:10
+xy = ShiftAOI(1, i, aoiinfo, driftlist);
+% The function output 0, 0 for anything outside range
+verifyEqual(testCase, xy, [0, 0]);
+end
+
+% Test for the frame we want to shift to is outside the driftlist frame
+% range
+aoiinfo = [5, 5, 0, 0, 5, 1];
+xy = ShiftAOI(1, 11, aoiinfo, driftlist);
+% The function output 0, 0 for anything outside range
+verifyEqual(testCase, xy, [0, 0]);
+
+% Test for driftlist does not start from frame 1
+driftlist(:, 1) = 2:11;
+aoiinfo = [5, 5, 0, 0, 5, 1];
+for i = 1:11
+    xy = ShiftAOI(1, i, aoiinfo, driftlist);
+    if i == 1
+        % The function output 0, 0 for anything outside range
+        verifyEqual(testCase, xy, [0, 0]);
+    else
+        verifyEqual(testCase, xy, [i-5, i-5]);
+    end
+end
+end
