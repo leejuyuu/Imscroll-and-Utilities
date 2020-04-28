b5c317da2cac5b4b8ff76fee7e53a892fef6bd8d
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-04-29 02:10:56 +0800

    Add another pickSpots test
    I made up a image with many gaussian peaks with different amplitude.
    Then I pick the spots and stored as test data in testPickSpots2.mat.

--- imscroll/test/testPickSpots.m
+++ imscroll/test/testPickSpots.m
@@ -12,3 +12,23 @@ function testPickSpots1(testCase)
     coords = pickSpots(zeros(256), spotPickingParam, {1,256,1,256});
     verifyEqual(testCase, coords, [])
 end
+
+function testPickSpots2(testCase)
+    
+    spotPickingParam = struct(...
+        'noiseDiameter',1,...
+        'spotDiameter',5,...
+        'spotBightness',50 ...
+        );
+    testImage = zeros(250);
+    gaussKernel = fspecial('gaussian', 5, 1.2);
+    for i = 1:50
+        for j = 1:50
+            testImage((i-1)*5+1:i*5, (j-1)*5+1:j*5) = 80 * (i + j) * gaussKernel;
+        end
+    end
+    
+    coords = pickSpots(testImage, spotPickingParam, {1,250,1,250});
+    coordsTrue = load('imscroll/test/test_data/testPickSpots2.mat').coords;
+    verifyEqual(testCase, coords, coordsTrue)
+end
